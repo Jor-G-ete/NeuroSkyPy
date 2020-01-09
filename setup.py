067a03d30f2077a08de6ac60247cdba05d325dfc
Jorge López Marcos <40428136+Jor-G-ete@users.noreply.github.com>
2020-01-09 01:05:51 +0100

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+ 
+setup(name = "NeuroPy",
+      version = "1.0",
+      description = "Library for interfacing with Neurosky's Mindwave EEG headset",
+      author = "Jorge Lopez Marcos",
+      author_email = "jlomar2005@hotmail.com",
+      url = "https://github.com/Jor-G-ete/NeuroPy",
+      packages=['NeuroPy']
+      )
