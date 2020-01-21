dff28db6579441a27366e15339193b972c4cdf84
Jorge López Marcos <40428136+Jor-G-ete@users.noreply.github.com>
2020-01-21 18:06:32 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from distutils.core import setup
  
-setup(name = "NeuroPy",
+setup(name = "NeuroSkyPy",
       version = "1.0",
       description = "Library for interfacing with Neurosky's Mindwave EEG headset",
       author = "Jorge Lopez Marcos",
       author_email = "jlomar2005@hotmail.com",
-      url = "https://github.com/Jor-G-ete/NeuroPy",
-      packages=['NeuroPy']
+      url = "https://github.com/Jor-G-ete/NeuroSkyPy",
+      packages=['NeuroSkyPy']
       )
