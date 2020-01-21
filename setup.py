328e4610b14c15afa14be00277f7a7c93f2eb7b1
Jorge Lopez <jorge.lopez@jot-im.com>
2020-01-21 19:05:37 +0100

    First version ready

--- setup.py
+++ setup.py
@@ -3,12 +3,13 @@ from distutils.core import setup
 setup(
       name="NeuroSkyPy",
       packages=['NeuroSkyPy'],
-      version="1.0",
+      version="1.1",
       license="GNU General Public License v3.0",
       description="Library for interfacing with Neurosky's Mindwave EEG headset",
       author="Jorge Lopez Marcos",
       author_email="jlomar2005@hotmail.com",
       url="https://github.com/Jor-G-ete/NeuroSkyPy",
+      download_url="https://github.com/Jor-G-ete/NeuroSkyPy/archive/v1.1.tar.gz",
       keyword=["python3.7", "NeuroSky", "graphics"],
       classifiers=[
             'Development Status :: 3 - Alpha',  # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
